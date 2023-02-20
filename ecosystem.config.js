d461d42c8457dfb07739113e568e6045f1d6b582
Prinya CHaitongrat <basprinya@outlook.com>
2023-02-20 13:37:57 +0700

    ok

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,17 +4,13 @@ module.exports = {
             name: "backend_api",
             script: "./app.js",
             watch: true,
-            dev: {
+            development: {
                 "PORT": 5000,
                 "NODE_ENV": "development"
             },
-            prod: {
+            production: {
                 "PORT": 5000,
                 "NODE_ENV": "production",
-            },
-            rp: {
-                "PORT": 5000,
-                "NODE_ENV": "rp",
             },
         }
     ]
