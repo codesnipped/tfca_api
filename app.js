5db388b1727a25445d5b18a658fc694113417042
Prinya CHaitongrat <basprinya@outlook.com>
2023-02-20 13:07:29 +0700

    ok

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ var httpsServer = https.createServer(options, app);
 
 const PORT = process.env.PORT || 5000
 httpServer.listen(PORT, () => {
-    const env = `${process.env.NODE_ENV || 'slave'}`
+    const env = `${process.env.NODE_ENV || 'prod'}`
     console.log(`App listening on port ${PORT}`)
     console.log(`App listening on env ${env}`)
     console.log(`Press Ctrl+C to quit.`)
